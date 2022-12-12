d1ff1fadbe1ca8b4a4e01b53df9508593eca5309
abhishekpaul594 <46164565+abhishekpaul594@users.noreply.github.com>
2022-12-12 13:39:00 +0530

    Add DriverUtil.java
    1. DriverUtil.java to initialize chromedriver
    2. Constants - introduce driverpath for different OS
    3. cucumber.properties - Remove browser properties

--- src/main/java/methods/Constants.java
+++ src/main/java/methods/Constants.java
@@ -3,12 +3,14 @@ package methods;
 public class Constants {
 
     public static final String CONFIGPATH="src/main/resources/cucumber.properties";
-    public static final String BROWSER = "browser";
     public static final String HEADLESS="headless";
     public static final String ENVIRONMENT="src/test/resources/inputdata/Environments.yaml";
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     public static final String DRIVERPATH = System.getProperty("user.dir") + FILE_SEPARATOR + "webdrivers";
-    public static final String CHROMEDRIVER_PATH = DRIVERPATH + FILE_SEPARATOR + "chromedriver.exe";
+    public static final String CHROMEDRIVER_PATH_WIN = DRIVERPATH + FILE_SEPARATOR + "chromedriver_win32/chromedriver.exe";
+    public static final String CHROMEDRIVER_PATH_MAC = DRIVERPATH + FILE_SEPARATOR + "chromedriver_mac64/chromedriver.exe";
+    public static final String CHROMEDRIVER_PATH_LIN = DRIVERPATH + FILE_SEPARATOR + "chromedriver_linux64/chromedriver.exe";
+
     public static final String PAGEOBJECT_PATH="src/test/resources/pageobject";
 
     public static final String INPUTDATA_PATH="src/test/resources/inputdata";
---
+++ src/main/java/methods/DriverUtil.java
@@ -0,0 +1,90 @@
+package methods;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import static methods.Constants.*;
+import static methods.GlobalProperties.getConfigProperties;
+
+public class DriverUtil {
+    private static WebDriver driver;
+    private static GlobalProperties configProperties = getConfigProperties();
+
+    //Initialize proper chrome driver in WebDriver object
+    public static WebDriver getDefaultDriver() {
+        if (driver != null && ((RemoteWebDriver) driver).getSessionId() != null)
+            return driver;
+        driver = chooseDriver();
+        return driver;
+    }
+
+    //Choosing appropriate chrome driver
+    private static WebDriver chooseDriver() {
+        RemoteWebDriver driver = null;
+        System.out.println("======Before drover created======");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        if (configProperties.getProperty(HEADLESS).equalsIgnoreCase("true")) {
+            chromeOptions.addArguments("--headless");
+        }
+        chromeOptions.addArguments("start-maximized");
+        chromeOptions.setExperimentalOption("excludeSwitches",
+                Collections.singletonList("enable-automation"));
+        String os = System.getProperty("os.name");
+        if (os.indexOf("win") >= 0) {
+            System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH_WIN);
+        } else if (os.indexOf("mac") >= 0) {
+            System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH_MAC);
+        } else if (os.indexOf("nux") >= 0) {
+            System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH_LIN);
+        }
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        System.out.println("########## After driver created ########");
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    //Wait until jquery js load completes
+    public static boolean waitForJqueryJsToLoad() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        ExpectedCondition<Boolean> jqueryLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                try {
+                    return ((boolean) ((JavascriptExecutor) driver).executeScript("return jQuery.active==0;"));
+                } catch (Exception e) {
+                    return true;
+                } finally {
+
+                }
+            }
+        };
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                try {
+                    return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+                } catch (Exception e) {
+                    return true;
+                } finally {
+
+                }
+            }
+        };
+        return wait.until(jqueryLoad) && wait.until(jsLoad);
+    }
+}
