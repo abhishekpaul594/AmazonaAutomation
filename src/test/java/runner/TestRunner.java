0319bb13b5b47a7f0470a43de7ae802aa672ec37
abhishekpaul594 <46164565+abhishekpaul594@users.noreply.github.com>
2022-12-12 17:38:15 +0530

    Update TestRunner.java
    Update glue property value in test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,5 @@
 package runner;
 
-import io.cucumber.java.After;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import methods.DriverUtil;
@@ -12,7 +11,7 @@ import java.io.FileNotFoundException;
 
 @CucumberOptions(tags="@test",
         features = {"src/test/resources/features"},
-        glue={"src/test/java/methods"},
+        glue={"methods"},
         plugin = {"pretty","html:target/cucumberHtmlReport.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
