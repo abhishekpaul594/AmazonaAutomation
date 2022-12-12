57085c027beecb4dd6432f3d3ef4e753c0a7b238
abhishekpaul594 <46164565+abhishekpaul594@users.noreply.github.com>
2022-12-12 15:27:33 +0530

    Create TestRunner.java

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,29 @@
+package runner;
+
+import io.cucumber.java.After;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import methods.DriverUtil;
+import methods.PageObjectGenerator;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.FileNotFoundException;
+
+@CucumberOptions(tags="@test",
+        features = {"src/test/resources/features"},
+        glue={"src/test/java/methods"},
+        plugin = {"pretty","html:target/cucumberHtmlReport.html"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    public static void setup() throws FileNotFoundException {
+        PageObjectGenerator.getPageObject();
+    }
+
+    @AfterClass
+    public static void destroy() {
+        DriverUtil.closeDriver();
+    }
+}
