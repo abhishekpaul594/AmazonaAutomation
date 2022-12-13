821a3db15dfde054d3d67435ca58b2da94be8e8b
abhishekpaul594 <46164565+abhishekpaul594@users.noreply.github.com>
2022-12-13 17:26:44 +0530

    Update chromedriver path for Linux and Mac system, headless=true

--- src/main/java/methods/Constants.java
+++ src/main/java/methods/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     public static final String DRIVERPATH = System.getProperty("user.dir") + FILE_SEPARATOR + "webdrivers";
     public static final String CHROMEDRIVER_PATH_WIN = DRIVERPATH + FILE_SEPARATOR + "chromedriver_win32/chromedriver.exe";
-    public static final String CHROMEDRIVER_PATH_MAC = DRIVERPATH + FILE_SEPARATOR + "chromedriver_mac64/chromedriver.exe";
-    public static final String CHROMEDRIVER_PATH_LIN = DRIVERPATH + FILE_SEPARATOR + "chromedriver_linux64/chromedriver.exe";
+    public static final String CHROMEDRIVER_PATH_MAC = DRIVERPATH + FILE_SEPARATOR + "chromedriver_mac64/chromedriver";
+    public static final String CHROMEDRIVER_PATH_LIN = DRIVERPATH + FILE_SEPARATOR + "chromedriver_linux64/chromedriver";
 
     public static final String PAGEOBJECT_PATH="src/test/resources/pageobject";
 
